d76c65e8f12f1e6436bcd80bcb930be36eb3caaa
justinji <justin5562587@gmail.com>
2020-09-30 18:26:30 +0800

    edit

--- main.cpp
+++ main.cpp
@@ -1,18 +1,32 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main() {
 
     sf::Window window(sf::VideoMode(500, 500), "My Window");
 
-    while(window.isOpen()) {
+//    sf::Texture texture;
+//    if (!texture.loadFromFile("../sfml_test.jpeg")) {
+//        return EXIT_FAILURE;
+//    }
+//    sf::Sprite sprite;
+//    sprite.setTexture(texture);
+
+    sf::Image image;
+    if (!image.loadFromFile("../sfml_test.jpeg")) {
+        return EXIT_FAILURE;
+    }
+
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+
+        window.display();
     }
 
     return 0;
